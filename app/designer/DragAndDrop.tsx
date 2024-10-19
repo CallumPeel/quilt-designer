1687a4d21a2fc17292182963fcd8c02b2c81f69d
slinky <callum.epeel@gmail.com>
2024-10-19 16:19:55 +0800

    feat: multiple droppable

--- app/designer/DragAndDrop.tsx
+++ app/designer/DragAndDrop.tsx
@@ -1,80 +1,85 @@
-import React from "react"
-import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core"
+import React from "react";
+import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
 
 interface DraggableProps {
-  name: string
-  id: string
+    name: string;
+    id: string;
+}
+
+interface DroppableProps {
+    isOver: boolean;
+    name: string;
+    id: string;
 }
 
 const DraggableItem = ({ id, name }: DraggableProps) => {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id,
-  })
+    const { attributes, listeners, setNodeRef, transform } = useDraggable({
+        id,
+    });
 
-  const style = {
-    transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
-    width: 100,
-    height: 100,
-    backgroundColor: "lightblue",
-    textAlign: "center",
-    lineHeight: "100px",
-    cursor: "grab",
-  }
+    const style = {
+        transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
+        width: 100,
+        height: 100,
+        backgroundColor: "lightblue",
+        textAlign: "center",
+        lineHeight: "100px",
+        cursor: "grab",
+    };
 
-  return (
-    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
-          { name }
-    </div>
-  )
-}
+    return (
+        <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+            {name}
+        </div>
+    );
+};
 
-const DroppableArea = ({ isOver }: { isOver: boolean }) => {
-  const { setNodeRef } = useDroppable({
-    id: "droppable",
-  })
+const DroppableArea = ({ name, id, isOver }: DroppableProps) => {
+    const { setNodeRef } = useDroppable({
+        id, // Unique ID for each droppable area
+    });
 
-  const style = {
-    width: 200,
-    height: 200,
-    backgroundColor: isOver ? "lightcoral" : "lightgreen",
-    textAlign: "center",
-    lineHeight: "200px",
-    transition: "background-color 0.2s",
-  }
+    const style = {
+        width: 200,
+        height: 200,
+        backgroundColor: isOver ? "lightcoral" : "lightgreen",
+        textAlign: "center",
+        lineHeight: "200px",
+        transition: "background-color 0.2s",
+    };
 
-  return (
-    <div ref={setNodeRef} style={style}>
-      Drop here
-    </div>
-  )
-}
+    return (
+        <div ref={setNodeRef} style={style}>
+            {name}
+        </div>
+    );
+};
 
 const DragAndDrop = () => {
-  const [isOver, setIsOver] = React.useState(false)
+    const [overId, setOverId] = React.useState<string | null>(null); // Track the ID of the droppable currently hovered
 
-  const handleDragEnd = (event: any) => {
-    if (event.over && event.over.id === "droppable") {
-      alert("Dropped in the correct zone!")
-    }
-  }
+    const handleDragEnd = (event: any) => {
+        if (event.over && event.over.id) {
+            alert(`Dropped on: ${event.over.id}`);
+        }
+        setOverId(null); // Reset after drop
+    };
 
-  const handleDragOver = (event: any) => {
-    setIsOver(event.over?.id === "droppable")
-  }
+    const handleDragOver = (event: any) => {
+        setOverId(event.over?.id || null); // Track the ID of the currently hovered drop area
+    };
 
-  const handleDragLeave = () => {
-    setIsOver(false)
-  }
+    return (
+        <DndContext onDragEnd={handleDragEnd} onDragOver={handleDragOver}>
+            <div style={{ display: "flex", justifyContent: "space-around", paddingTop: "50px" }}>
+                <DraggableItem id="drag1" name="squarey1" />
+                <DraggableItem id="drag2" name="squarey2" />
 
-  return (
-    <DndContext onDragEnd={handleDragEnd} onDragOver={handleDragOver} onDragLeave={handleDragLeave}>
-      <div style={{ display: "flex", justifyContent: "space-around", paddingTop: "50px" }}>
-        <DraggableItem id="1" name="square1" />
-        <DraggableItem id="2" name="square2" />
-        <DroppableArea isOver={isOver} />
-      </div>
-    </DndContext>
-  )
-}
+                <DroppableArea id="drop1" name="droppy1" isOver={overId === "drop1"} />
+                <DroppableArea id="drop2" name="droppy2" isOver={overId === "drop2"} />
+            </div>
+        </DndContext>
+    );
+};
 
-export default DragAndDrop
+export default DragAndDrop;
